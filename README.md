765a42a4e125c21f42cad0e2013a72147fd59a2b
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-02-08 17:20:14 +0100

    Update KaTeX to 0.12.0 and automate the download of KaTeX files
    Also add the missing KaTeX fonts and add a CSP to the add-on after auto
    formatting the manifest.

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Render LaTeX math parts in your markdown files when viewing them on GitHub.
    1. Run the [`package_extension.sh`](package_extension.sh) script
 2. Load extension (multiple possibilities)
    1. Temporary (is deleted after restart)
-      1. Go to `about:addons` and click the icon at the top right with the option `Debug Add-on`
+      1. Go to `about:addons` and click the icon at the top right with the option `Debug Add-ons`
       2. Click `Load Temporary Add-on` and select the created `.zip` archive
    2. Create *non public* signed Add-on **OR** *public* signed Add-on
       1. Follow this tutorial: [Submitting an add-on](https://extensionworkshop.com/documentation/publish/submitting-an-add-on/#self-distribution)
+
+## Update KaTeX files
+
+Run the [`download_katex_files.sh`](download_katex_files.sh) script.
---
+++ download_katex_files.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+KATEX_VERSION="0.12.0"
+BASE_URL="https://cdn.jsdelivr.net/npm/katex@${KATEX_VERSION}/dist"
+KATEX_LOCAL_DIR="katex"
+KATEX_FILES=(
+    katex.min.js
+    katex.min.css
+)
+KATEX_CONTRIB_FILES=(
+    auto-render.min.js
+)
+
+KATEX_FONTS_BASE_URL="https://github.com/KaTeX/katex-fonts/tree/master/fonts"
+KATEX_FONTS_LOCAL_DIR="${KATEX_LOCAL_DIR}/fonts"
+KATEX_FONTS=(
+    KaTeX_AMS-Regular.woff2
+    KaTeX_Caligraphic-Bold.woff2
+    KaTeX_Caligraphic-Regular.woff2
+    KaTeX_Fraktur-Bold.woff2
+    KaTeX_Fraktur-Regular.woff2
+    KaTeX_Main-Bold.woff2
+    KaTeX_Main-Italic.woff2
+    KaTeX_Main-Regular.woff2
+    KaTeX_Math-BoldItalic.woff2
+    KaTeX_Math-Italic.woff2
+    KaTeX_Math-Regular.woff2
+    KaTeX_SansSerif-Bold.woff2
+    KaTeX_SansSerif-Italic.woff2
+    KaTeX_SansSerif-Regular.woff2
+    KaTeX_Script-Regular.woff2
+    KaTeX_Size1-Regular.woff2
+    KaTeX_Size2-Regular.woff2
+    KaTeX_Size3-Regular.woff2
+    KaTeX_Size4-Regular.woff2
+    KaTeX_Typewriter-Regular.woff2
+)
+
+# Clean up previously downloaded files
+rm -rf "${KATEX_LOCAL_DIR}"
+
+mkdir -p "${KATEX_LOCAL_DIR}"
+# Download the KaTeX CSS and JS file
+LICENSE_LIST_KATEX_FILES=""
+for KATEX_FILE in "${KATEX_FILES[@]}"; do
+    wget -O "${KATEX_LOCAL_DIR}/${KATEX_FILE}" "${BASE_URL}/${KATEX_FILE}"
+    LICENSE_LIST_KATEX_FILES="${LICENSE_LIST_KATEX_FILES}${KATEX_FILE}, "
+done
+# Download the KaTeX contrib files (auto renderer)
+for KATEX_CONTRIB_FILE in "${KATEX_CONTRIB_FILES[@]}"; do
+    wget -O "${KATEX_LOCAL_DIR}/${KATEX_CONTRIB_FILE}" "${BASE_URL}/contrib/${KATEX_CONTRIB_FILE}"
+    LICENSE_LIST_KATEX_FILES="${LICENSE_LIST_KATEX_FILES}${KATEX_CONTRIB_FILE}, "
+done
+
+mkdir -p "${KATEX_FONTS_LOCAL_DIR}"
+# Download fonts used by KaTeX
+LICENSE_LIST_KATEX_FONT_FILES=""
+for KATEX_FONT in "${KATEX_FONTS[@]}"; do
+    wget -O "${KATEX_FONTS_LOCAL_DIR}/${KATEX_FONT}" "${KATEX_FONTS_BASE_URL}/${KATEX_FONT}"
+    LICENSE_LIST_KATEX_FONT_FILES="${LICENSE_LIST_KATEX_FONT_FILES}${KATEX_FONT}, "
+done
+
+echo "The files ${LICENSE_LIST_KATEX_FILES::-2} are taken from \"${BASE_URL}\" and are used under the terms of the MIT license: https://github.com/KaTeX/KaTeX/blob/master/LICENSE.
+
+The files ${LICENSE_LIST_KATEX_FONT_FILES::-2} are taken from \"${KATEX_FONTS_BASE_URL}\" and are used under the terms of the MIT license: https://github.com/KaTeX/KaTeX/blob/master/LICENSE." > "${KATEX_LOCAL_DIR}/LICENSE"
