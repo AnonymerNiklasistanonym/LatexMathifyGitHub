7d3b485d1fba270e753773dfbe31086eecfa43d4
jiangy <jiangyi15@mails.ucas.ac.cn>
2021-02-08 21:08:04 +0800

    support gitlab like math code

--- enableMarkdownLatexMath.js
+++ enableMarkdownLatexMath.js
@@ -1,8 +1,16 @@
+function get_markdown_node() {
+    var node = document.getElementsByClassName("markdown-body")[0];
+    return node.parentNode;
+}
+
+
 const renderMath = () => {
-	const elementToRender = document.getElementById("readme");
+	const elementToRender = get_markdown_node();
 	if (elementToRender) {
+        var markdown = get_markdown_node();
+        markdown.innerHTML = markdown.innerHTML.replace(/<pre lang="math"><code>(.+)\n<\/code><\/pre>/g, "$$$$ $1 $$$");
 		console.debug("Render Readme");
-		renderMathInElement(document.getElementById("readme"), {
+		renderMathInElement(get_markdown_node(), {
 			delimiters: [{
 				left: "$$",
 				right: "$$",
