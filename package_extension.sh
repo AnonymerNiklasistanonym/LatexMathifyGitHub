14a2ad8a0b78d7556f29f6d1e3d29e74fdacb085
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2022-03-02 10:59:40 +0100

    Update packaging by removing unnecessary files

--- package_extension.sh
+++ package_extension.sh
@@ -4,7 +4,13 @@ mkdir -p dist
 
 rm -f manifest.json
 cp manifest.firefox.json manifest.json
-zip -r -FS extension_firefox.zip * --exclude '*.git*' --exclude 'README*' --exclude '*.zip' --exclude '*.sh' --exclude '*test*' --exclude '*dist*' --exclude '*manifest.*.json'
+zip -r -FS extension_firefox.zip * \
+    --exclude '*.git*' --exclude 'README*' \
+    --exclude '*.zip' --exclude '*.sh' \
+    --exclude '*test*' --exclude '*delete*' --exclude '*dist*' \
+    --exclude '*katex/fonts/*'  --exclude '*katex/auto-render.min.js' \
+    --exclude '*icons/icon.svg' \
+    --exclude '*manifest.*.json'
 cp extension_firefox.zip dist/extension_firefox.zip
 rm -rf dist/extension_firefox
 unzip dist/extension_firefox.zip -d dist/extension_firefox
@@ -12,7 +18,13 @@ rm manifest.json
 
 rm -f manifest.json
 cp manifest.chromium.json manifest.json
-zip -r -FS extension_chromium.zip * --exclude '*.git*' --exclude 'README*' --exclude '*.zip' --exclude '*.sh' --exclude '*test*' --exclude '*dist*' --exclude '*katex/fonts/*' --exclude '*manifest.*.json'
+zip -r -FS extension_chromium.zip * \
+    --exclude '*.git*' --exclude 'README*' \
+    --exclude '*.zip' --exclude '*.sh' \
+    --exclude '*test*' --exclude '*delete*' --exclude '*dist*' \
+    --exclude '*katex/fonts/*' --exclude '*katex/auto-render.min.js' \
+    --exclude '*icons/icon.svg' \
+    --exclude '*manifest.*.json'
 cp extension_chromium.zip dist/extension_chromium.zip
 rm -rf dist/extension_chromium
 unzip dist/extension_chromium.zip -d dist/extension_chromium
